6e233d8b47204b23440d1c6668570d2e2f97766b
ramsabah <ramsabah28@gmail.com>
2025-08-04 12:42:29 +0200

    implementing the model for the category

---
+++ lib/src/models/CategoryModel.dart
@@ -0,0 +1,11 @@
+class CategoryModel {
+  final String name;
+  final String description;
+  final String imageUrl;
+
+  const CategoryModel({
+    required this.name,
+    required this.description,
+    required this.imageUrl,
+  });
+}
