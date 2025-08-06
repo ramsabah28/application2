82982f1e2475319b94152f1463b305d541bcb0fc
ramsabah <ramsabah28@gmail.com>
2025-08-06 14:44:46 +0200

    update DynamicProductList as placeholder

---
+++ lib/src/component/DynamicProductList.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DynamicProductList extends StatelessWidget {
+  const DynamicProductList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Hello World',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
