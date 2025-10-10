ba346f9084e727c063a801e815ff3c4c15bdcad3
ramsabah <ramsabah28@gmail.com>
2025-10-10 11:03:14 +0200

    apply migration changes

---
+++ lib/src/services/AdressService.dart
@@ -0,0 +1,10 @@
+import 'package:application2/src/models/AdressModel.dart';
+import 'package:flutter/material.dart';
+
+class AdressService {
+  static Future<void> addAdressData(AdressModel model) async {
+
+
+
+  }
+}
