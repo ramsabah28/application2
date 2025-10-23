472f3e9b99431b372fb02c82ac0ca4a64f851ddb
ramsabah <ramsabah28@gmail.com>
2025-10-23 16:39:02 +0200

    imrove changes

--- lib/src/component/homeScreenFeatures/SaleCard.dart
+++ lib/src/component/homeScreenFeatures/SaleCard.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/material.dart';
 
-class SaleCards extends StatelessWidget {
+class SaleCards extends StatefulWidget {
   const SaleCards({Key? key}) : super(key: key);
+
+  @override
+  State<SaleCards> createState() => _SaleCardsState();
+}
+
+class _SaleCardsState extends State<SaleCards>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+      lowerBound: 0.9,
+      upperBound: 1.1,
+    )..repeat(reverse: true); // Loops forever
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.red,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       margin: const EdgeInsets.only(top: 1, left: 10, right: 10),
+      clipBehavior: Clip.antiAlias,
       child: SizedBox(
         height: 200,
-        child: Center(
-          child: Text(
-            '30% Off',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('lib/assets/3dGirl.png'),
+              fit: BoxFit.cover,
+              alignment: Alignment(0.0, -0.96),
+            ),
+          ),
+          child: Center(
+            child: ScaleTransition(
+              scale: _controller,
+              child: const Text(
+                '30% Off',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                  shadows: [
+                    Shadow(
+                      color: Colors.black45,
+                      offset: Offset(2, 6),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
